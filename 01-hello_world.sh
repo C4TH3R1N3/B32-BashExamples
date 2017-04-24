975d2d974ea0980b35b990bec62fb01616bdf326
Catherine Giroux <cath3rine.gir0ux@gmail.com>
2017-04-24 17:35:10 -0400

    01-hello-world.sh

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+echo "salut"
+#exit
+echo "toi"
+
+# -n : pas de retour de ligne
+echo -n "Vive"
+echo " Linux "
+
+# -e = Accepter les méta-caractères (\n, \t, etc...)
+echo -e "1\n2\n3"
+
+# -p = une phrase sera demandée à l'utilisateur
+# Pour une variable : si $ devant on veut accéder à sa valeur
+# sinon : on affecte la variable
+read -p "Quel âge avez-vous?" age
+echo $age "ans! Vous paraissez jeune!"
