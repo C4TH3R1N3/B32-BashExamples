ad9ca00fd07c16254bdd6b25d9104ae7a2fd2ba7
Catherine Giroux <cath3rine.gir0ux@gmail.com>
2017-04-26 16:47:23 -0400

    Exercice synthèse 3 terminé

---
+++ exercice3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "@"
+
+FaireEspaces() {
+	nb=$1
+	i=0
+
+	while test $i -lt $nb
+	do
+		i=$(($i+1))
+		echo -n " "
+	done
+}
+
+for ((i=0;i<30;i++))
+do
+	clear	
+	FaireEspaces $i
+	echo "@"
+	sleep 0.1
+done
+
+clear
+FaireEspaces 30
+echo "BOOM"
