223543caa1094993726a1a3e57918e0ec92773c3
Catherine Giroux <cath3rine.gir0ux@gmail.com>
2017-04-26 15:24:07 -0400

    Exercice 1 en cours

--- Exercice 1.sh
+++ Exercice 1.sh
@@ -3,3 +3,7 @@
 # La ligne suivante permet de vérifier si un mot existe dans le dictionnaire
 # Il faut suffixer cet URL avec le mot entré par l'uasger (ex: word=chat)
 #wget -qO - http://dictionary.objectif8.com/exists.php?word=
+
+read -p "Ã‰crivez un mot :" mot
+echo $mot
+ 
